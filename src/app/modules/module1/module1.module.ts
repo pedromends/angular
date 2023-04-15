bbc3a5f1049018ad79037f91705a7a8eb3c36c18
pedro <joaopedromend14@gmail.com>
2023-04-15 15:18:01 -0300

    go

---
+++ src/app/modules/module1/module1.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Module1RoutingModule } from './module1-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    Module1RoutingModule
+  ]
+})
+export class Module1Module { }
---
+++ src/app/modules/module3/module3.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Module3RoutingModule } from './module3-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    Module3RoutingModule
+  ]
+})
+export class Module3Module { }
